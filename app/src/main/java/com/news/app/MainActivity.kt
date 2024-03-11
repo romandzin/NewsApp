b247c5a23fa45e226bbff9a5a636c84bf98b6877
root <redrenjer@yandex.ru>
2024-03-11 09:46:17 +0300

    Version 0.1 SplashScreen Complete

--- app/src/main/java/com/news/app/MainActivity.kt
+++ app/src/main/java/com/news/app/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.news.app
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewAnimationUtils
+import android.widget.ImageView
+import android.widget.Toolbar
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import androidx.core.view.isVisible
+import com.airbnb.lottie.LottieAnimationView
+import com.news.app.databinding.ActivityMainBinding
+import java.time.Clock
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import kotlin.math.max
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        splashScreen.setOnExitAnimationListener { vp ->
+            val lottieView = findViewById<LottieAnimationView>(R.id.animationView)
+            lottieView.enableMergePathsForKitKatAndAbove(true)
+            lottieView.postDelayed({
+                vp.view.alpha = 0f
+                vp.iconView.alpha = 0f
+                lottieView!!.playAnimation()
+            }, 1000)
+
+            lottieView.addAnimatorListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    binding.toolbar.toolbarActionbar.isVisible = true
+                    setTheme(R.style.Base_Theme_NewsApp_NewTheme)
+                    binding.root.setBackgroundColor(resources.getColor(R.color.white, theme))
+                    binding.animationView.visibility = View.GONE
+                    setSupportActionBar(binding.toolbar.toolbarActionbar)
+                }
+            })
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        finish()
     }
 }
